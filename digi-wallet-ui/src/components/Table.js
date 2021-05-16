858efbeb671d0584177e1637afa8501e0c767b01
Vishnu Divakar <vishnudivakar31@gmail.com>
2021-05-16 13:50:37 -0400

    cancel transaction and styles

--- digi-wallet-ui/src/components/Table.js
+++ digi-wallet-ui/src/components/Table.js
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react'
-import { useTable } from 'react-table'
+import { useTable, usePagination} from 'react-table'
 
 export default function Table(props) {
     const data = props.tableData
@@ -11,54 +11,110 @@ export default function Table(props) {
         getTableProps,
         getTableBodyProps,
         headerGroups,
-        rows,
         prepareRow,
-      } = useTable({ columns, data })
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        state: { pageIndex, pageSize }
+      } = useTable({ columns, data, initialState: { pageIndex: 0 } }, usePagination)
 
     return (
-        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-            <thead>
+        <div>
+            <table {...getTableProps()}>
+                <thead>
                 {headerGroups.map(headerGroup => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
+                    <tr {...headerGroup.getHeaderGroupProps()}>
                     {headerGroup.headers.map(column => (
-                    <th
-                        {...column.getHeaderProps()}
-                        style={{
-                        borderBottom: 'solid 3px red',
-                        background: 'aliceblue',
-                        color: 'black',
-                        fontWeight: 'bold',
-                        }}
-                    >
-                        {column.render('Header')}
-                    </th>
-                    ))}
-                </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
-                prepareRow(row)
-                return (
-                    <tr {...row.getRowProps()}>
-                    {row.cells.map(cell => {
-                        return (
-                        <td
-                            {...cell.getCellProps()}
+                        <th 
+                            {...column.getHeaderProps()}
                             style={{
-                            padding: '10px',
-                            border: 'solid 1px gray',
-                            background: 'papayawhip',
+                                padding: '1% 0.5%',
+                                background: '#192a56',
+                                color: 'white',
+                                fontWeight: 'bold',
+                                whiteSpace: 'nowrap'
                             }}
                         >
-                            {cell.render('Cell')}
-                        </td>
-                        )
-                    })}
+                            {column.render('Header')}
+                        </th>
+                    ))}
                     </tr>
-                )
+                ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                {page.map((row, i) => {
+                    prepareRow(row)
+                    return (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map(cell => {
+                            return (
+                                <td 
+                                    {...cell.getCellProps()}
+                                    style={{
+                                        padding: '1% 0.5%',
+                                        background: '#dfe4ea',
+                                        textAlign: 'center'
+                                    }}
+                                >
+                                    {cell.render('Cell')}
+                                </td>
+                            )
+                        })}
+                    </tr>
+                    )
                 })}
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+            <div className="pagination" style={{ marginTop: '1%' }}>
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                {'>>'}
+                </button>{' '}
+                <span>
+                Page{' '}
+                <strong>
+                    {pageIndex + 1} of {pageOptions.length}
+                </strong>{' '}
+                </span>
+                <span>
+                | Go to page:{' '}
+                <input
+                    type="number"
+                    defaultValue={pageIndex + 1}
+                    onChange={e => {
+                    const page = e.target.value ? Number(e.target.value) - 1 : 0
+                    gotoPage(page)
+                    }}
+                    style={{ width: '100px' }}
+                />
+                </span>{' '}
+                <select
+                value={pageSize}
+                onChange={e => {
+                    setPageSize(Number(e.target.value))
+                }}
+                >
+                {[10, 20, 30, 40, 50].map(pageSize => (
+                    <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                    </option>
+                ))}
+                </select>
+            </div>
+        </div>
     )
 }
